95d3c3d2bf81fc5f3aac2b5c56b5326e730a8944
Dan Kary <dankary@gmail.com>
2021-12-01 12:39:16 -0500

    more testing planning

--- tests/testthat/test-nodes.R
+++ tests/testthat/test-nodes.R
@@ -1,17 +1,19 @@
 library(dfgraph)
 
+# TODO:
+# - The context is probably parse_nodes()
+
 test_that("Normal expressions are parsed into 1 node row each", {
     
 })
 
-test_that("A if/else expression is parsed into multiple node rows", {
+test_that("An if/else expression is parsed into multiple node rows", {
     
 })
 
 test_that("If/else symmetric nodes are correctly identified", {
     
 })
-
 
 test_that("If/else asymmetric nodes are correctly identified", {
     
--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -1,5 +1,14 @@
 library(dfgraph)
 
+# TODO: 
+# - think about whether you really need these tests
+#   I think it's worthwhile (these seem to be "external" enough behaviors)
+# - The context is probably parse_expression() so consider renaming this file to reflect
+
+# - Maybe consider starting with more complex funcs (e.g., get_depends)
+#   Correctly identifying dependencies is the primary role of this package
+#   and I can capture a variety of scenarios by testing that one function
+
 test_that("'assign' value is correctly parsed from expression", {
     
 })
@@ -7,7 +16,6 @@ test_that("'assign' value is correctly parsed from expression", {
 test_that("'function' value is correctly parsed from expression", {
     
 })
-
 
 test_that("'member' value is correctly parsed from expression", {
     

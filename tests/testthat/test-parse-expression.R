7ba98bbfd1e16891ac43d7eb9109174175140939
Dan Kary <dankary@gmail.com>
2021-12-03 12:31:07 -0500

    setup failing tests for parse_expression() ifelse

--- tests/testthat/test-parse-expression.R
+++ tests/testthat/test-parse-expression.R
@@ -19,9 +19,41 @@ test_that("values are correctly parsed for a simple expression", {
     expect_col_equal(x, "person", col = "member")
 })
 
-test_that("values are correctly parsed for an if/else expression", {
-    # These will return multiple dataframe rows for one expression
-    # 1. symmetric nodes (e.g, assignment in all conditions)
-    # 2. assymetric nodes (e.g., assignment in a subset of conditions)
-    
+test_that("values are correctly parsed for an assymetric if/else expression", {
+    # The if/else resolves to completely different expressions in this case, b/c
+    # a. There are no assignments of the same name in the different paths, and
+    # b. There are no equivalent function operation in the different paths
+    x <- quote(
+        if (param == 1) {
+            a <- f(b)
+        } else {
+            b <- f(a)
+        }
+    )
+    expect_col_equal(x, c("a", "b"), col = "assign")
+    expect_col_equal(x, c("f", "f"), col = "function")
+    # TODO: the string expectation is clunky, will probably want to check
+    # equivalence in expression
+    expect_col_equal( 
+        x, c(
+            "if (param == 1) {\na <- f(b)\n}", 
+            "if (param == 1) {\n} else {\nb <- f(a)\n}"
+        ), col = "code"
+    )
+})
+
+test_that("values are correctly parsed for a symmetric if/else expression", {
+    # The if/else should resolve to one expression in this case b/c we have
+    # exactly one assignment regardless of the condition
+    x <- quote(
+        if (param == 1) {
+            a <- f(b)
+        } else {
+            a <- f1(b)
+        }
+    )
+    expect_col_equal(x, "a", col = "assign")
+    expect_col_equal(x, c("f or f1"), col = "function")
+    expect_col_equal(x, "if param == 1 {\na <- f(b)\n} {\na <- f1(b)\n}", 
+                     col = "code")
 })

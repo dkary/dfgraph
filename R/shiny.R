c70256c2079653dcfe3e9f4b7c8cb628e6eef817
Dan Kary <dankary@gmail.com>
2021-12-06 21:38:50 -0500

    shiny planning

---
+++ R/shiny.R
@@ -0,0 +1,6 @@
+# bring in a shiny app for more fully-featured interactivity
+
+# TODO:
+# - implement graph() args as selection menus
+# - build a second panel (to one side) for displaying network code
+# - double-click on a node to display the corresponding network code
